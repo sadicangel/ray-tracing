b4d20160fb766b39cbb75e6963101367826b475e
sadicangel <sadicangel@hotmail.com>
2025-03-23 23:32:25 +0000

    Add background image support and update ray casting to use dynamic background colors

--- src/main.zig
+++ src/main.zig
@@ -4,9 +4,9 @@ const vec = @import("vec.zig");
 
 const WINDOW_WIDTH = 1024;
 const WINDOW_HEIGHT = 768;
+const POSITION = Vec3f{ 0, 0, 0 };
 const FOV: f32 = std.math.pi / 2.0;
 const MAX_DEPTH = 4;
-const BACKGROUND_COLOR = Vec3f{ 0.2, 0.7, 0.8 };
 
 const Vec3f = @Vector(3, f32);
 const Vec4f = @Vector(4, f32);
@@ -86,6 +86,7 @@ const Light = struct {
 };
 
 const Scene = struct {
+    background: []Vec3f,
     objects: std.ArrayList(Object) = std.ArrayList(Object).init(std.heap.page_allocator),
     lights: std.ArrayList(Light) = std.ArrayList(Light).init(std.heap.page_allocator),
 
@@ -104,20 +105,20 @@ const Scene = struct {
     }
 };
 
-fn castRay(scene: Scene, rayOrigin: Vec3f, rayDirection: Vec3f, depth: usize) Vec3f {
+fn castRay(scene: Scene, background: Vec3f, rayOrigin: Vec3f, rayDirection: Vec3f, depth: usize) Vec3f {
     var hit: Vec3f = undefined;
     var normal: Vec3f = undefined;
     var object: Object = undefined;
     if (depth > MAX_DEPTH or !scene.intersect(rayOrigin, rayDirection, &hit, &normal, &object)) {
-        return BACKGROUND_COLOR;
+        return background;
     }
 
     const reflectDirection: Vec3f = vec.normalize(vec.reflect(rayDirection, normal));
     const reflectOrigin: Vec3f = if (vec.dot(reflectDirection, normal) < 0.0) hit - vec.scale(normal, 1e-3) else hit + vec.scale(normal, 1e-3);
-    const reflectColor = castRay(scene, reflectOrigin, reflectDirection, depth + 1);
+    const reflectColor = castRay(scene, background, reflectOrigin, reflectDirection, depth + 1);
     const refractDirection: Vec3f = vec.normalize(vec.refract(rayDirection, normal, object.material.refactiveIndex));
     const refractOrigin: Vec3f = if (vec.dot(refractDirection, normal) < 0.0) hit - vec.scale(normal, 1e-3) else hit + vec.scale(normal, 1e-3);
-    const refractColor = castRay(scene, refractOrigin, refractDirection, depth + 1);
+    const refractColor = castRay(scene, background, refractOrigin, refractDirection, depth + 1);
 
     var diffuseLightIntensity: f32 = 0.0;
     var specularLightIntensity: f32 = 0.0;
@@ -162,12 +163,13 @@ fn render(scene: Scene) !void {
 
     for (0..WINDOW_HEIGHT) |j| {
         for (0..WINDOW_WIDTH) |i| {
-            var color3f = castRay(scene, Vec3f{ 0, 0, 0 }, getDirection(i, j), 0);
+            const offset = j * WINDOW_WIDTH + i;
+            var color3f = castRay(scene, scene.background[offset], POSITION, getDirection(i, j), 0);
             const max: f32 = @max(color3f[0], color3f[1], color3f[2]);
             if (max > 1) {
                 color3f = vec.scale(color3f, 1 / max);
             }
-            frameBuffer[i + j * WINDOW_WIDTH] = vec.toColor(color3f, 255);
+            frameBuffer[offset] = vec.toColor(color3f, 255);
         }
     }
 
@@ -177,15 +179,13 @@ fn render(scene: Scene) !void {
 pub fn main() !void {
     stb.init(std.heap.page_allocator);
 
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const background = try stb.Image.loadFromFile("./src/background.jpg", 0);
 
     const ivory = Material{ .albedo = Vec4f{ 0.6, 0.3, 0.1, 0.0 }, .diffuseColor = Vec3f{ 0.4, 0.4, 0.3 }, .specularExponent = 50.0, .refactiveIndex = 1.0 };
     const glass = Material{ .albedo = Vec4f{ 0.0, 0.5, 0.1, 0.8 }, .diffuseColor = Vec3f{ 0.6, 0.7, 0.8 }, .specularExponent = 125.0, .refactiveIndex = 1.5 };
     const redRubber = Material{ .albedo = Vec4f{ 0.9, 0.1, 0.0, 0.0 }, .diffuseColor = Vec3f{ 0.3, 0.1, 0.1 }, .specularExponent = 10.0, .refactiveIndex = 1.0 };
     const mirror = Material{ .albedo = Vec4f{ 0.0, 10.0, 0.8, 0.0 }, .diffuseColor = Vec3f{ 1.0, 1.0, 1.0 }, .specularExponent = 1425.0, .refactiveIndex = 1.0 };
-    // brown carpet material
-    const carpet = Material{ .albedo = Vec4f{ 0.1, 0.025, 0.0, 0.0 }, .diffuseColor = Vec3f{ 0.5, 0.3, 0.1 }, .specularExponent = 0.0, .refactiveIndex = 0.0 };
+    const carpet = Material{ .albedo = Vec4f{ 0.1, 0.025, 0.0, 0.04 }, .diffuseColor = Vec3f{ 0.5, 0.3, 0.1 }, .specularExponent = 0.0, .refactiveIndex = 0.0 };
 
     const plane = Object{
         .geometry = Geometry{ .Plane = Plane{ .planeNormal = .{ 0, 1, 0 }, .planeOffset = 4 } },
@@ -216,7 +216,18 @@ pub fn main() !void {
     const light2 = Light{ .position = Vec3f{ 30, 50, -25 }, .intensity = 1.8 };
     const light3 = Light{ .position = Vec3f{ 30, 20, 30 }, .intensity = 1.7 };
 
-    var scene = Scene{};
+    const imgDataF32 = try std.heap.page_allocator.alloc(Vec3f, WINDOW_WIDTH * WINDOW_HEIGHT);
+    defer std.heap.page_allocator.free(imgDataF32);
+
+    for (0..WINDOW_WIDTH * WINDOW_HEIGHT) |i| {
+        const r: f32 = @floatFromInt(background.data[i * 3 + 0]);
+        const g: f32 = @floatFromInt(background.data[i * 3 + 1]);
+        const b: f32 = @floatFromInt(background.data[i * 3 + 2]);
+        imgDataF32[i] = .{ r / 255.0, g / 255.0, b / 255.0 };
+    }
+
+    var scene = Scene{ .background = imgDataF32 };
+
     try scene.objects.append(plane);
     try scene.objects.append(sphere1);
     try scene.objects.append(sphere2);
